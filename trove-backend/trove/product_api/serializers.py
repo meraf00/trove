582b04efa56eb438013d5dd17d6008f936380789
Leul Wujira <meraf000@gmail.com>
2023-11-03 21:22:40 +0300

    feat: add image serializer

--- trove-backend/trove/product_api/serializers.py
+++ trove-backend/trove/product_api/serializers.py
@@ -1,15 +1,24 @@
 from rest_framework import serializers
-from .models import Product, Category
+from .models import Product, Category, Image
 
 class CategroySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ('name',)
+    
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = ('image_url',)
 
 
 class ProductSerializer(serializers.ModelSerializer):
     categories = CategroySerializer(many=True)
+    images = ImageSerializer(many=True)
 
     class Meta:
         model = Product
-        fields = ('id', 'name', 'price', 'stock', 'image_url', 'categories',)
+        fields = ('id', 'name', 'price', 'stock', 'description', 'images', 'categories',)
+
+
